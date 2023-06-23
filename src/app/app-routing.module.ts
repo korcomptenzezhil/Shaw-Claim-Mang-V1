8a36cfb0f5c45cebc72c915b07c0967c8fe2bd26
Ezhil-87 <babue@korcomptenz.com>
2023-06-23 19:40:50 +0530

    CSS to SCSS Convert Fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { InitiateClaimComponent } from './initiate-claim/initiate-claim.component';
+import { ReportsComponent } from './reports/reports.component';
+import { WorkQueueComponent } from './work-queue/work-queue.component';
+import { SettingsComponent } from './settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'initiateClaim', component: InitiateClaimComponent },
+  { path: 'reports', component: ReportsComponent },
+  { path: 'workQueue', component: WorkQueueComponent },
+  { path: 'settings', component: SettingsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/initiate-claim/initiate-claim.component.ts
@@ -0,0 +1,109 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-initiate-claim',
+  templateUrl: './initiate-claim.component.html',
+  styleUrls: ['./initiate-claim.component.css']
+})
+export class InitiateClaimComponent {
+  openModal() {
+    const modelDiv = document.getElementById('staticBackdrop');
+    if (modelDiv != null) {
+      modelDiv.style.display = 'block';
+    }
+  }
+
+  openModalStopInitiate() {
+    const modelDiv = document.getElementById('stopinitiate');
+    if (modelDiv != null) {
+      modelDiv.style.display = 'block';
+    }
+  }
+
+  openModalInitiateClaim() {
+    const modelDiv = document.getElementById('initiateclaim');
+    if (modelDiv != null) {
+      modelDiv.style.display = 'block';
+    }
+  }
+
+  openModalEndUserInfo() {
+    const modelDiv = document.getElementById('enduserinfo');
+    if (modelDiv != null) {
+      modelDiv.style.display = 'block';
+    }
+  }
+
+
+
+  openModalPriorClaimSummary() {
+    const modelDiv = document.getElementById('priorclaimsummary');
+    if (modelDiv != null) {
+      modelDiv.style.display = 'block';
+    }
+  }
+  openModalAddInvoice() {
+    const modelDiv = document.getElementById('addinvoice');
+    if (modelDiv != null) {
+      modelDiv.style.display = 'block';
+    }
+  }
+
+  CloseModal() {
+    const modelDiv = document.getElementById('staticBackdrop');
+    if (modelDiv != null) {
+      modelDiv.style.display = 'none';
+    }
+  }
+
+  CloseModalStopInitiate() {
+    const modelDiv = document.getElementById('stopinitiate');
+    if (modelDiv != null) {
+      modelDiv.style.display = 'none';
+    }
+  }
+
+
+  CloseModalInitiateClaim() {
+    const modelDiv = document.getElementById('initiateclaim');
+    if (modelDiv != null) {
+      modelDiv.style.display = 'none';
+    }
+  }
+
+
+  CloseModalEndUserInfo() {
+    const modelDiv = document.getElementById('enduserinfo');
+    if (modelDiv != null) {
+      modelDiv.style.display = 'none';
+    }
+  }
+
+
+  CloseModalPriorClaimSummary() {
+    const modelDiv = document.getElementById('priorclaimsummary');
+    if (modelDiv != null) {
+      modelDiv.style.display = 'none';
+    }
+  }
+
+  CloseModalAddInvoice() {
+    const modelDiv = document.getElementById('addinvoice');
+    if (modelDiv != null) {
+      modelDiv.style.display = 'none';
+    }
+  }
+
+  ngOnInit() {
+    this.addBodyClass();
+  }
+  // Function: Add class to body 
+  addBodyClass() {
+    const bodyTag = document.body;
+    bodyTag.classList.add('my-class');
+  }
+
+
+}
+
+
